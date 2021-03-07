60e18f1d542a1d5d9eded9e052b8cc493c0b8c09
crashdemons <crashenator@gmail.com>
2021-03-06 23:10:48 -0600

    rebuild in release mode

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ mkdir dist
 
 BIN="blake224.cpp blake256.cpp blake384.cpp blake512.cpp"
 
-#W_FLAGS="-O3 -flto -DNDEBUG"
-W_FLAGS="-O0 -g -v -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 --memoryprofiler"
+W_FLAGS="-O3 -flto -DNDEBUG"
+#W_FLAGS="-O0 -g -v -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 --memoryprofiler"
 echo $W_FLAGS
 echo $BIN
 emcc $W_FLAGS -s MODULARIZE=1 -s "EXPORTED_FUNCTIONS=['_malloc','_free']" -s 'EXPORT_NAME="createBlakeModule"' -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' $BIN blake_easy_stub.cpp -o dist/blake.js
