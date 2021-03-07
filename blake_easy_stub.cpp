9a809d1df01c9fea48367a6ba7dd3d5521c41bce
crashdemons <crashenator@gmail.com>
2021-03-06 23:07:54 -0600

    undo debug lines

--- blake_easy_stub.cpp
+++ blake_easy_stub.cpp
@@ -1,41 +1,41 @@
 #include "blake_easy_stub.hpp"
 
 #include <iostream>
-
+/*
 void* mallocz(size_t size){
 	void* p = malloc(size);
 	memset(p, 0, size);
 	return p;
 }
-
+*/
 blake_state_easy* blake_init_easy(unsigned int digestBits){
-	blake_state_easy* ctx = (blake_state_easy*) mallocz(sizeof(blake_state_easy));
+	blake_state_easy* ctx = (blake_state_easy*) malloc(sizeof(blake_state_easy));
 	ctx->bitlength = digestBits;
 	switch(ctx->bitlength){
 		case 224:
-			ctx->internal_state = mallocz(sizeof(state224));
+			ctx->internal_state = malloc(sizeof(state224));
 			blake224_init((state224*) ctx->internal_state);
 			break;
 		case 256:
-			ctx->internal_state = mallocz(sizeof(state256));
+			ctx->internal_state = malloc(sizeof(state256));
 			blake256_init((state256*) ctx->internal_state);
 			break;
 		case 384:
-			ctx->internal_state = mallocz(sizeof(state384));
+			ctx->internal_state = malloc(sizeof(state384));
 			blake384_init((state384*) ctx->internal_state);
 			break;
 		case 512:
 		default:
 			ctx->bitlength = 512;//if it wasn't already set to this
-			ctx->internal_state = mallocz(sizeof(state512));
+			ctx->internal_state = malloc(sizeof(state512));
 			blake512_init((state512*) ctx->internal_state);
 			break;
 	}
-	std::cout<<"init-easy "<< (ctx) << " " << (ctx->bitlength) << " " << (ctx->internal_state) <<std::endl;
+//	std::cout<<"init-easy "<< (ctx) << " " << (ctx->bitlength) << " " << (ctx->internal_state) <<std::endl;
 	return ctx;
 }
 void blake_update_easy(blake_state_easy* ctx, const uint8_t *in, uint64_t inlen){
-	std::cout<<"update-easy "<< ctx <<" "<< (ctx->bitlength) << " " << in << " " << inlen <<std::endl;
+//	std::cout<<"update-easy "<< ctx <<" "<< (ctx->bitlength) << " " << in << " " << inlen <<std::endl;
 	switch(ctx->bitlength){
 		case 224:
 			return blake224_update((state224*)ctx->internal_state,in,inlen);
@@ -48,7 +48,7 @@ void blake_update_easy(blake_state_easy* ctx, const uint8_t *in, uint64_t inlen)
 	}
 }
 void blake_final_easy(blake_state_easy* ctx, uint8_t *out){
-	std::cout<<"final-easy "<< ctx <<" "<< (ctx->bitlength)<< " " << out <<std::endl;
+//	std::cout<<"final-easy "<< ctx <<" "<< (ctx->bitlength)<< " " << out <<std::endl;
 	switch(ctx->bitlength){
 		case 224:
 			return blake224_final((state224*)ctx->internal_state,out);
@@ -62,11 +62,11 @@ void blake_final_easy(blake_state_easy* ctx, uint8_t *out){
 }
 
 void blake_cleanup_easy(blake_state_easy* ctx){
-	std::cout<<"cleanup-easy "<< ctx <<" "<< (ctx->bitlength) <<std::endl;
+//	std::cout<<"cleanup-easy "<< ctx <<" "<< (ctx->bitlength) <<std::endl;
 	free(ctx->internal_state);
 	free(ctx);
 }
 
 int version(){
-    return 2021030603;
+    return 2021030604;
 }
